9cb007de72ac57ea9760d6c271f4837ebfdce881
amegeddon <ameescook@gmail.com>
2024-07-22 23:18:10 +0100

    Corrects typo from import env

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from environs import Env
+from environs import env
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
